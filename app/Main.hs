892c21cb7f2c32839d74ca3765fdcfbba8e2a426
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2024-01-24 18:20:20 +0200

    add func signature

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import qualified Chapter1.Index as C1
 (#) :: (Show a) => String -> a -> IO ()
 (#) label a = print $ (label <> ": " <> show a)
 
+printChapter :: Int -> IO ()
 printChapter n = putStrLn $ title <> underline
  where
   title = "\nChapter " <> padding <> "\n"
@@ -18,11 +19,14 @@ printChapter n = putStrLn $ title <> underline
 
 main :: IO ()
 main = do
+  -- Chapter 1
   printChapter 1
   "curried 2 3" # C1.curried 2 3
   "uncurried (2, 3)" # C1.uncurried (2, 3)
   "factoriel 5" # C1.factoriel 5
   "fibonacci 10" # C1.fibs 10
-  printChapter 2
+
+-- Chapter 2
+-- printChapter 2
 
 -- print $ concatMap' (* 10) [[1, 2], [3, 4], [5, 6, 7, 8]]
