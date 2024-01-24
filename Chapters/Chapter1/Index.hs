aafadf01e4301f5c6e60b6493f9fd9cc25534db7
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2024-01-24 17:35:56 +0200

    reexport modules for Chapter 1 && 2

---
+++ Chapters/Chapter1/Index.hs
@@ -0,0 +1,9 @@
+module Chapter1.Index (
+    module Chapter1.CurryUncurry,
+    module Chapter1.Factoriel,
+    module Chapter1.Fibonacci,
+) where
+
+import Chapter1.CurryUncurry
+import Chapter1.Factoriel
+import Chapter1.Fibonacci
---
+++ Chapters/Chapter2/Index.hs
@@ -0,0 +1,9 @@
+module Chapter2.Index (
+    module Chapter2.ConcatMap,
+    module Chapter2.ReverseList,
+    module Chapter2.ZipWith,
+) where
+
+import Chapter2.ConcatMap
+import Chapter2.ReverseList
+import Chapter2.ZipWith
