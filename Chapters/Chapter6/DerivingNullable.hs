82c32f5cd1730776ec004ae6bb6a951e9570415c
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2024-01-29 20:44:06 +0200

    Chapter 6 - Refactor Deriving Nullable

--- Chapters/Chapter6/DerivingNullable.hs
+++ Chapters/Chapter6/DerivingNullable.hs
@@ -25,32 +25,31 @@ instance Nullable (Maybe a) where
     null :: Maybe a
     null = Nothing
 
-newtype MaybeEmptyList a = MaybeEmptyList (Maybe [a])
+newtype BasicNullable = BasicNullable
+    {getString :: Maybe String}
+    deriving (Nullable) via (Maybe String)
 
-instance Nullable (MaybeEmptyList a) where
-    isNull :: MaybeEmptyList a -> Bool
-    isNull (MaybeEmptyList Nothing) = True
-    isNull (MaybeEmptyList (Just x)) =
+newtype TransitiveNullable = TransitiveNullable
+    {getNonEmptyString :: Maybe String}
+
+instance Nullable TransitiveNullable where
+    isNull :: TransitiveNullable -> Bool
+    isNull (TransitiveNullable Nothing) = True
+    isNull (TransitiveNullable (Just x)) =
         case x of
             [] -> True
             _ -> False
 
-    null :: MaybeEmptyList a
-    null = MaybeEmptyList Nothing
-
-newtype EmptyListFalseNull a = EmptyListFalseNull (Maybe a)
-    deriving (Nullable) via (Maybe a)
-
-newtype EmptyListTrueNull a = EmptyListTrueNull (MaybeEmptyList a)
-    deriving (Nullable) via (MaybeEmptyList a)
+    null :: TransitiveNullable
+    null = TransitiveNullable Nothing
 
 main = do
-    let emptyListFalseNull = EmptyListFalseNull (Just [])
-    let emptyListFalseNull2 = EmptyListFalseNull (Just [1, 2, 3])
-    let emptyListTrueNull = EmptyListTrueNull (MaybeEmptyList (Just []))
-    let emptyListTrueNull2 = EmptyListTrueNull (MaybeEmptyList (Just [1, 2, 3]))
+    let basicNullable = BasicNullable (Just "")
+    let basicNullable2 = BasicNullable (Just "abc")
+    let transitiveNullable = TransitiveNullable (Just "")
+    let transitiveNullable2 = TransitiveNullable (Just "abc")
 
-    print $ isNull emptyListFalseNull
-    print $ isNull emptyListFalseNull2
-    print $ isNull emptyListTrueNull
-    print $ isNull emptyListTrueNull2
+    print $ isNull basicNullable
+    print $ isNull basicNullable2
+    print $ isNull transitiveNullable
+    print $ isNull transitiveNullable2
