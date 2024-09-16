5b591a3c59236d72922f8111aad026ec9a1707ae
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2024-09-16 18:38:35 +0300

    Chapter 7 changes

--- Chapters/Chapter7/NestedIOActions.hs
+++ Chapters/Chapter7/NestedIOActions.hs
@@ -1,38 +1,24 @@
 module Chapter7.NestedIOActions where
 
-import Control.Monad (join)
+typeOfNestedIoActions :: String -> IO (IO String)
+typeOfNestedIoActions = return . return
+-- print the string:
+-- typeOfNestedIoActions "abc" >>= (\x -> x) >>= print
 
-nestedIOActions :: a -> IO (IO a)
-nestedIOActions = return . return
+fromNestedIoActions :: IO (IO a) -> IO a
+fromNestedIoActions a = a >>= id
+-- print the value of `a`:
+-- fromNestedIoActions (return . return $ 123) >>= print
 
-printNestedIOActions :: IO (IO String) -> IO ()
-printNestedIOActions io = do
-    outer <- io
-    inner <- outer
-    putStrLn inner
+toListIoActions :: [a] -> [IO a]
+toListIoActions (x : xs) = return x : toListIoActions xs
 
-fromNestedIOActions :: IO (IO a) -> IO a
-fromNestedIOActions io = do join io
+fromListIoActions :: [IO a] -> IO [a]
+fromListIoActions = foldr combine (return [])
+    where
+        combine io acc = io
+            >>= \x -> acc
+            >>= \xs -> return (x : xs)
 
-toListIOActions :: [a] -> [IO a]
-toListIOActions (x : xs) = return x : toListIOActions xs
 
-fromListIOActions :: [IO a] -> IO [a]
-fromListIOActions = sequence
 
-printString :: IO String -> IO ()
-printString io = do
-    string <- io
-    putStrLn string
-
--- A function of type IO (IO String)
--- which prints the string:
--- printNestedIOActions $ nestedIOActions s
-
--- What happens if you try to use (>>=)?
--- nestedIOActions s >>= printString
-
--- A function that has the type signature: IO (IO a) -> IO a
--- (fromNestedIOActions . nestedIOActions $ s) >>= putStrLn
-
--- putStrLn $ toListIOActions . fromListIOActions $ [1 .. 3]
