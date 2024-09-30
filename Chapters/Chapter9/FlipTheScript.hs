9fbc1400dbc25acd4c8f31e7151c30023aa574da
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2024-09-30 18:49:03 +0300

    add chapter 9

---
+++ Chapters/Chapter9/FlipTheScript.hs
@@ -0,0 +1,30 @@
+module Chapter9.FlipTheScript where
+
+import Prelude hiding ((<*>), pure)
+
+-- Functor is defined in terms of Applicative
+class Applicative' f =>
+      Functor' f
+    where
+    fmap' :: (a -> b) -> f a -> f b
+    fmap' f e = pure f <*> e
+
+-- Applicative is defined in terms of Monad
+class Monad m =>
+      Applicative' m
+    where
+    pure :: a -> m a
+    pure = return
+    (<*>) :: m (a -> b) -> m a -> m b
+    mf <*> ma = mf >>= \f -> ma >>= \a -> return (f a)
+
+-- Functor' Isntance for List
+instance Functor' []
+
+-- Applicative' Isntance for List
+instance Applicative' []
+
+main :: IO ()
+main = do
+    print $ fmap' (+ 1) [1 .. 3]
+    print $ [(* 1), (* 2)] <*> [1 .. 3]
