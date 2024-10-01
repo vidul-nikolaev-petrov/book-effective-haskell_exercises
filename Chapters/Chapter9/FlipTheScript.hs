73b3800fc9add04166bb85332eab6449e6a02afa
Vidul Nikolaev Petrov <vidul.petrov@gmail.com>
2024-10-01 03:06:17 +0300

    exclude Predule

--- Chapters/Chapter9/FlipTheScript.hs
+++ Chapters/Chapter9/FlipTheScript.hs
@@ -1,13 +1,13 @@
 module Chapter9.FlipTheScript where
 
-import Prelude hiding ((<*>), pure)
+import Prelude hiding ((<*>), fmap, pure)
 
 -- Functor is defined in terms of Applicative
 class Applicative' f =>
       Functor' f
     where
-    fmap' :: (a -> b) -> f a -> f b
-    fmap' f e = pure f <*> e
+    fmap :: (a -> b) -> f a -> f b
+    fmap f e = pure f <*> e
 
 -- Applicative is defined in terms of Monad
 class Monad m =>
@@ -26,5 +26,5 @@ instance Applicative' []
 
 main :: IO ()
 main = do
-    print $ fmap' (+ 1) [1 .. 3]
+    print $ fmap (+ 1) [1 .. 3]
     print $ [(* 1), (* 2)] <*> [1 .. 3]
